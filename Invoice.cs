fb9e4b227efddd771154cd4a8f3adba96da17f91
Andriy Rachenko <108069918+Andrew1929@users.noreply.github.com>
2022-10-03 01:41:39 +0300

    Add files via upload

---
+++ Invoice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LB_3._4
+{
+    class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+        private string article;
+        private int quantity;
+        private int price;
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+        } 
+        public void PriceCalculator()
+        {
+
+            Console.Write("Enter article : ");
+            article = Console.ReadLine();
+            Console.Write("Enter price : ");
+            price = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter quantity : ");
+            quantity  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+            double priceWithoutPDF = price * quantity;
+            double priceWithtPDF = priceWithoutPDF / 100 * 20 + priceWithoutPDF;
+            Console.WriteLine($"Account #{account}; Custome - {customer} ; Provider - {provider}");
+            Console.WriteLine($"You buy {article} for the sum :{priceWithtPDF} grn (price with PDF)\n" +
+                $"Price without PDF : {priceWithoutPDF} grn");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP_LB_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(3432, "Andriy Rachenko", "Andriy Artemenko");
+            invoice.PriceCalculator();
+            Console.ReadKey();
+        }
+    }
+}
